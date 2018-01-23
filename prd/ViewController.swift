cfa6d3b451c7fcdb3e4b0e5a0514354270934613
Janis <182.dude@gmail.com>
2018-01-23 07:34:55 +0100

    Initial Commit

---
+++ prd/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  prd
+//
+//  Created by Jan Catlos on 23/01/2018.
+//  Copyright © 2018 Jan Catlos. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
